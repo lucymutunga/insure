0ecc9c53f453f5c6e2e1d8afc858b5578eda6d1c
lucymutunga <lucyalphonce18@gmail.com>
2024-01-15 10:39:11 +0300

    fixing config

---
+++ insure/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./components/Nav";
+const App = () => {
+  return (
+    <>
+      <h1>Testing</h1>
+      <Nav />;
+    </>
+  );
+};
+
+export default App;
---
+++ insure/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../images/logo.svg";
+
+const Nav = () => {
+  return (
+    <Header>
+      <h1>Testing</h1>
+      <Nav>
+        <div>
+          <img src={logo} alt="logo"></img>
+        </div>
+      </Nav>
+    </Header>
+  );
+};
+
+export default Nav;
